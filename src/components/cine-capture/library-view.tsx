1b18f0d347459bf76e115e1ec9a6e898d2ecea66
Hugues R <hugues.rabier@gmail.com>
2025-12-03 08:45:20 +0000

    double la taille des vignettes

--- src/components/cine-capture/library-view.tsx
+++ src/components/cine-capture/library-view.tsx
@@ -290,7 +290,7 @@ function LibraryViewContent() {
              </div>
           </div>
           {filteredItems.length > 0 ? (
-            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 md:gap-6">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6">
               {filteredItems.map(item => (
                 <MovieCard key={item.id} item={item} />
               ))}
