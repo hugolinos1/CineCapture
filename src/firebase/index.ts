8c471a4a0176e29b793e54b5d87e81ad962d5914
Hugues R <hugues.rabier@gmail.com>
2025-12-03 13:44:42 +0000

    Set up a Firebase backend

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -42,11 +42,9 @@ export function getSdks(firebaseApp: FirebaseApp) {
 
 export * from './provider';
 export * from './client-provider';
-export * from './firestore/use-collection.tsx';
-export * from './firestore/use-doc.tsx';
-export * from './auth/use-user';
+export * from './firestore/use-collection';
+export * from './firestore/use-doc';
 export * from './non-blocking-updates';
 export * from './non-blocking-login';
 export * from './errors';
 export * from './error-emitter';
-
